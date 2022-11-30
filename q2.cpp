4fdd5d3c5131f1d95400cc4b206f6284c6d26f95
david campbell <davidpcampbell2004@gmail.com>
2022-11-29 17:55:03 -0800

    answer

--- q2.cpp
+++ q2.cpp
@@ -2,12 +2,38 @@
 #include <iomanip>
 using namespace std;
 
-// ******************************
-// Your code here
-// ******************************
+void getInput(int &,int &,int &);
+int  findMin(int, int, int, int);
+void printResult(int,int,int,int);
 
-// ******************************
-// this function will help you pass the test. When you print the result with this function, you will get the same output format
+int main()
+{
+	int min, n1, n2, n3;
+	getInput(n1, n2, n3);
+	min = findMin(n1, n2, n3,min);
+	printResult(n1, n2, n3, min);
+}
+
+void getInput(int &n1, int &n2, int &n3)
+{
+	cout << "Enter three values, one at a time: " << endl;
+	cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int n1, int n2, int n3, int min)
+{
+	int array[3] = {n1,n2,n3};
+	min = array[0];
+	for(int i = 1; i < 3; i++)
+	{
+		if(array[i] < min)
+		{
+			min = array[i];
+		}
+	}
+	return min;
+}
+
 void printResult(int n1, int n2, int n3, int min)
 {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
